64719dd37d91563f62204f2179f1de0258230899
TaoLe <hopthucualetao@gmail.com>
2017-02-10 23:05:25 +0700

    province table

---
+++ province.sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 18, 2017 at 10:34 PM
+-- Server version: 5.7.16-0ubuntu0.16.04.1
+-- PHP Version: 7.0.14-2+deb.sury.org~xenial+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vietnam`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `province`
+--
+
+CREATE TABLE `province` (
+  `provinceid` varchar(5) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `type` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `province`
+--
+
+INSERT INTO `province` (`provinceid`, `name`, `type`) VALUES
+('01', 'Hà Nội', 'Thành Phố'),
+('02', 'Hà Giang', 'Tỉnh'),
+('04', 'Cao Bằng', 'Tỉnh'),
+('06', 'Bắc Kạn', 'Tỉnh'),
+('08', 'Tuyên Quang', 'Tỉnh'),
+('10', 'Lào Cai', 'Tỉnh'),
+('11', 'Điện Biên', 'Tỉnh'),
+('12', 'Lai Châu', 'Tỉnh'),
+('14', 'Sơn La', 'Tỉnh'),
+('15', 'Yên Bái', 'Tỉnh'),
+('17', 'Hòa Bình', 'Tỉnh'),
+('19', 'Thái Nguyên', 'Tỉnh'),
+('20', 'Lạng Sơn', 'Tỉnh'),
+('22', 'Quảng Ninh', 'Tỉnh'),
+('24', 'Bắc Giang', 'Tỉnh'),
+('25', 'Phú Thọ', 'Tỉnh'),
+('26', 'Vĩnh Phúc', 'Tỉnh'),
+('27', 'Bắc Ninh', 'Tỉnh'),
+('30', 'Hải Dương', 'Tỉnh'),
+('31', 'Hải Phòng', 'Thành Phố'),
+('33', 'Hưng Yên', 'Tỉnh'),
+('34', 'Thái Bình', 'Tỉnh'),
+('35', 'Hà Nam', 'Tỉnh'),
+('36', 'Nam Định', 'Tỉnh'),
+('37', 'Ninh Bình', 'Tỉnh'),
+('38', 'Thanh Hóa', 'Tỉnh'),
+('40', 'Nghệ An', 'Tỉnh'),
+('42', 'Hà Tĩnh', 'Tỉnh'),
+('44', 'Quảng Bình', 'Tỉnh'),
+('45', 'Quảng Trị', 'Tỉnh'),
+('46', 'Thừa Thiên Huế', 'Tỉnh'),
+('48', 'Đà Nẵng', 'Thành Phố'),
+('49', 'Quảng Nam', 'Tỉnh'),
+('51', 'Quảng Ngãi', 'Tỉnh'),
+('52', 'Bình Định', 'Tỉnh'),
+('54', 'Phú Yên', 'Tỉnh'),
+('56', 'Khánh Hòa', 'Tỉnh'),
+('58', 'Ninh Thuận', 'Tỉnh'),
+('60', 'Bình Thuận', 'Tỉnh'),
+('62', 'Kon Tum', 'Tỉnh'),
+('64', 'Gia Lai', 'Tỉnh'),
+('66', 'Đắk Lắk', 'Tỉnh'),
+('67', 'Đắk Nông', 'Tỉnh'),
+('68', 'Lâm Đồng', 'Tỉnh'),
+('70', 'Bình Phước', 'Tỉnh'),
+('72', 'Tây Ninh', 'Tỉnh'),
+('74', 'Bình Dương', 'Tỉnh'),
+('75', 'Đồng Nai', 'Tỉnh'),
+('77', 'Bà Rịa - Vũng Tàu', 'Tỉnh'),
+('79', 'Hồ Chí Minh', 'Thành Phố'),
+('80', 'Long An', 'Tỉnh'),
+('82', 'Tiền Giang', 'Tỉnh'),
+('83', 'Bến Tre', 'Tỉnh'),
+('84', 'Trà Vinh', 'Tỉnh'),
+('86', 'Vĩnh Long', 'Tỉnh'),
+('87', 'Đồng Tháp', 'Tỉnh'),
+('89', 'An Giang', 'Tỉnh'),
+('91', 'Kiên Giang', 'Tỉnh'),
+('92', 'Cần Thơ', 'Thành Phố'),
+('93', 'Hậu Giang', 'Tỉnh'),
+('94', 'Sóc Trăng', 'Tỉnh'),
+('95', 'Bạc Liêu', 'Tỉnh'),
+('96', 'Cà Mau', 'Tỉnh');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `province`
+--
+ALTER TABLE `province`
+  ADD PRIMARY KEY (`provinceid`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
