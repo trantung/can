314480a2a3585e28d07c61f21ceb1cdb78f61701
tungtt2 <tungtt2@hocmai.vn>
2017-02-25 23:09:14 +0700

    fix database

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => '127.0.0.1',
 			'database'  => 'hr',
 			'username'  => 'root',
-			'password'  => '123456',
+			'password'  => 'root',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
