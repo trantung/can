bbb4dea7f82808547bc675065ed3353be9f991df
2nd <haind@steed.vn>
2017-03-01 20:42:03 +0700

    fix log out catch permission

--- app/controllers/admin/AdminController.php
+++ app/controllers/admin/AdminController.php
@@ -2,7 +2,7 @@
 class AdminController extends BaseController {
     public function __construct() {
         $this->beforeFilter('admin', array('except'=>array('login','doLogin')));
-        $this->beforeFilter('checkPermission', array('except'=>array('login','doLogin')));
+        $this->beforeFilter('checkPermission', array('except'=>array('login','doLogin','logout')));
     }
     /**
      * Display a listing of the resource.
