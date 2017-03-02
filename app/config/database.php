18b46c469da3d397486f04c1f6811fc632a00f1b
tungtt2 <tungtt2@hocmai.vn>
2017-03-02 20:11:15 +0700

    open_permission

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => '127.0.0.1',
 			'database'  => 'hr',
 			'username'  => 'root',
-			'password'  => 'root',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
