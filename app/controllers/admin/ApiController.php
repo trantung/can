ec6d2886de6667760a75d33c7ca44a8e90b8ca73
tungtt2 <trantunghn196@gmail.com>
2017-05-12 22:10:05 +0700

    fix

--- app/controllers/admin/ApiController.php
+++ app/controllers/admin/ApiController.php
@@ -68,9 +68,6 @@ class ApiController extends BaseController {
                 return Response::json($response);
             }
         }
-        
-        ScaleManage::where('scale_station_code', $codeScaleStation)
-                    ->where('app_id', $appId)->update(['active', 2]);
         $id = ScaleManage::create(['scale_station_code' => $codeScaleStation, 'app_id' => $appId, 'active' => 1])->id;
         // $dataInsert['scale_station_code'] = $codeScaleStation;
         // $dataInsert['app_id'] = $appId;
