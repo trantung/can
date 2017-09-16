91efa4e8db09208aee8f2755d553fa060286fd73
tungtt2 <trantunghn196@gmail.com>
2017-09-16 15:22:24 +0700

    test shipchung lan 2

--- app/routes.php
+++ app/routes.php
@@ -22,6 +22,7 @@ Route::get('/', function(){
 Route::post('/testshipchung', function(){
     $input = Input::all();
     // dd($input);
+    $input = json_encode($input);
     DB::table('shipchung')->insert([
         'log' => $input 
     ]);
