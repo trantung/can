4e0c644b4c206b454e0f375b4a73616096da3979
Tan Nguyen <tantanb2@gmail.com>
2017-11-24 22:23:22 +0700

    Fix select all warehouseID and cutomerID

--- app/views/admin/scale-station/template/search-scale-odd-js.blade.php
+++ app/views/admin/scale-station/template/search-scale-odd-js.blade.php
@@ -4,14 +4,14 @@
 		$(document).on('change', 'select#department_id', function(e){
 			var department_id = $(this).val();
 			$('select#warehouse_id').val('').change();
-			$('select#warehouse_id option').hide();
+			$('select#warehouse_id option:not(:first)').hide();
 			$('select#warehouse_id option[department-id="'+ department_id +'"]').show();
 		})
 
 		$(document).on('change', 'select#customer_group_id', function(e){
 			var customer_group_id = $(this).val();
 			$('select#customer_id').val('').change();
-			$('select#customer_id option').hide();
+			$('select#customer_id option:not(:first)').hide();
 			$('select#customer_id option[customer-groups-id="'+ customer_group_id +'"]').show();
 		})
 	})
